78e88e10f985c8f625212cf8ffc083fbf1ec1e7f
rsolsy7-commits <rsolsy7@gmail.com>
2026-05-11 18:15:13 +0300

    Update config.js

--- setup/config.js
+++ setup/config.js
@@ -2,10 +2,10 @@ export default {
   "prefix": "!",  // Bot prefix for command invocation
   "language": "vi_VN",  // Language setting for the bot
   "BOT_NAME": "Thieu Trung Kien",  // The name of the bot
-  "ADMIN_IDS": ["100056992160405"],  // Admin IDs who have special privileges
+  "ADMIN_IDS": ["61562975344669"],  // Admin IDs who have special privileges
 
   // Bot configuration options
-  "options": {
+  "options": {61562975344669]
     "forceLogin": true,  // Force the bot to log in
     "listenEvents": true,  // Listen for events
     "listenTyping": false,  // Disable listening to typing events
